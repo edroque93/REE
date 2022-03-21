d1de0882a5f75e6e8fed82b154af4755f9571316
edroque93 <quique.lpgc@gmail.com>
2022-03-21 18:38:48 +0000

    Cleaning + always fetch up to previous day

--- main.py
+++ main.py
@@ -3,19 +3,19 @@
 from src.logger import logger
 from src.processors import *
 from src.pvpc import pvpc
-from datetime import date
+from datetime import date, timedelta
 
 def main():
     log = logger()
-    log.start("ree.log", False)
+    log.start('ree.log', False)
     try:
-        data = pvpc.fetch(date(2014, 4, 1), date(2022, 3, 17))
-        # plot_maxweekly(data)
-        # print_csv(data)
+        data = pvpc.fetch(date(2014, 4, 1), date.today() - timedelta(days=1))
+        plot_maxweekly(data)
+        print_csv(data)
         plot_limits(data)
     except Exception as e:
         log.error(str(e))
     log.stop()
 
-if __name__== "__main__":
+if __name__== '__main__':
     main()
